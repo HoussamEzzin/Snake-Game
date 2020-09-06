d01673e7c22de345c5915e5a9776fec2ae860cb0
HoussamEzzin <ezzinhoussam@gmail.com>
2020-09-06 13:34:37 +0100

    creating class Food.

--- game.js
+++ game.js
@@ -19,3 +19,20 @@ function init(){
     ctx=canvas.getContext("2d");
     
 }
+class Food{
+    constructor(pos,color){
+        this.x=pos.x;
+        this.y=pos.y;
+        this.color=color;
+    }
+    draw(){
+        ctx.beginPath();
+        ctx.rect(this.x,this.y,tileSize,tileSize);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+        ctx.strokeStyle = "black";
+        ctx.lineWidth=3;
+        ctx.stroke();
+        ctx.closePath();
+    }
+}
